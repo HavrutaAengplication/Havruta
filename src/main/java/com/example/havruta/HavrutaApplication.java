7977c40ab98494856bbdff4c137be57d79f9df92
Ho-hoho <80965670+Ho-hoho@users.noreply.github.com>
2023-01-30 14:16:57 +0900

    Update HavrutaApplication.java

--- src/main/java/com/example/havruta/HavrutaApplication.java
+++ src/main/java/com/example/havruta/HavrutaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HavrutaApplication {
-
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(HavrutaApplication.class, args);
 	}
