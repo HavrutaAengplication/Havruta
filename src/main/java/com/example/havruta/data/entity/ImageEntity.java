e0f83bf81821e790844d016e01d87703ce941c11
Ho-hoho <kennyya@naver.com>
2023-02-13 21:00:13 +0900

    add entities

---
+++ src/main/java/com/example/havruta/data/entity/ImageEntity.java
@@ -0,0 +1,10 @@
+package com.example.havruta.data.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ImageEntity {
+    @Id
+    private String image;
+}
