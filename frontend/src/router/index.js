0cece19aa622fce399a6a3dedab1a44982b19c87
Jaewoong Jung <jaewoong_jung@naver.com>
2023-02-27 20:36:46 +0900

    GroupPage Test removed

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,10 +2,9 @@ import {createRouter, createWebHistory} from "vue-router";
 import MainPage from "@/components/MainPage.vue";
 import MyPage from "@/components/MyPage.vue";
 import Login from "@/components/KaKaoLogin.vue"
-import GroupPageTest from "@/components/GroupPageTest.vue";
+import GroupPage from "@/components/GroupPage.vue";
 // import CategoryPageTest from "@/components/CategoryPageTest.vue";
 import MyProblems from "@/components/MyProblems.vue"
-import GroupPage from "@/components/GroupPage.vue"
 // import CreateProblem from "@/components/CreateProblem.vue";
 //import CreateGroup from "@/components/CategoryView.vue"
 import CategoryProblems from "@/components/CategoryProblems.vue"
@@ -14,7 +13,7 @@ const routes = [
     { path: "/home", name: "Home", component: MainPage},
     { path: "/mypage", name: "MyPage", component: MyPage },
     { path: "/login", name: "Login", component: Login},
-    { path: '/groups/:groupId', component: GroupPageTest },
+    { path: '/groups/:groupId', component: GroupPage },
     { path: '/groups/:groupId/categories/:categoryId', component: CategoryProblems},
 
     { path: "/problems", name: "MyProblem", component: MyProblems},
